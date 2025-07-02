39d7e1307ddbdba94e035fa8683f75654729a0a0
GAURI ARORA <reachgauriarora@gmail.com>
2025-07-02 17:16:25 +0530

    Improved main.py: added input and function structure

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+def main():
 print("Hello world")
 print("sdc assignment 1")
+name = input("Enter your name: ")
+print(f" {name}, welcome to SDC Assignment 1!")
+if __name__ == "__main__":
+    main()
